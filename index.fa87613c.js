715542ca4a28a95bfd282221958fb02c9435dc9d
Alexander Ruppert <alexander.ruppert@rwth-aachen.de>
2020-11-30 22:59:28 +0100

    Updates

---
+++ index.fa87613c.js
@@ -0,0 +1,1 @@
+[].find||(document.body.innerHTML="Sorry, but ancient browsers, like Internet Explorer, are not supported.");
--- noIE.ec675d7d.js
+++
@@ -1,2 +0,0 @@
-[].find||(document.body.innerHTML="Sorry, but ancient browsers, like Internet Explorer, are not supported.");
-//# sourceMappingURL=noIE.ec675d7d.js.map
