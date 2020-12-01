8026cdbbdb671f306da5b49136772aa2decd69f7
Alexander Ruppert <alexander.ruppert@rwth-aachen.de>
2020-12-01 22:04:26 +0100

    massively optimized generated svg size

--- src/scripts/svg/renderer.ts
+++ src/scripts/svg/renderer.ts
@@ -1,50 +1,25 @@
-interface Position {
-  x: number
-  y: number
-}
-function createElement(tag: string) {
-  return document.createElementNS('http://www.w3.org/2000/svg', tag)
-}
-
-const debug = true
-function createRectTemplate() {
-  let rect = createElement('rect')
-
-  rect.setAttribute('width', '1')
-  rect.setAttribute('height', '1')
-
-  return rect
-}
-const rectTemplate = createRectTemplate()
-
-function createRect(container: DocumentFragment, position: Position) {
-  const rect = rectTemplate.cloneNode(false) as SVGElement
-  rect.setAttribute('x', position.x.toString())
-  rect.setAttribute('y', position.y.toString())
-
-  container.appendChild(rect)
-}
-
 export function render(canvas: SVGSVGElement, matrix: boolean[][]) {
-  canvas.innerHTML = ''
-  canvas.setAttribute(
-    'viewBox',
-    `0 0 ${matrix[0].length + 8 || 10} ${matrix.length + 8}`,
-  )
-  const g = createElement('g')
-  g.setAttribute('fill', '#000')
-  g.setAttribute('shape-rendering', 'crispEdges')
+  const viewBox = matrix.length + 10
+  canvas.setAttribute('viewBox', `0 0 ${viewBox} ${viewBox}`)
 
-  const fragment = document.createDocumentFragment()
+  let d = ''
   for (let y = 0; y < matrix.length; y++) {
-    for (let x = 0; x < matrix[y].length; x++) {
-      if (matrix[y][x] === true) {
-        createRect(fragment, { x: x + 4, y: y + 4 })
+    let first = true
+    let lastX = 0
+    for (let x = 0; x < matrix.length; x++) {
+      if (matrix[y][x]) {
+        let len = 0
+        while (++len + x < matrix.length && matrix[y][len + x]);
+
+        d +=
+          (first && !(first = false)
+            ? `M${x + 5} ${y + 5}`
+            : `m${x - lastX} 0`) + `h${len}`
+        lastX = (x += len - 1) + 1
       }
     }
   }
-  g.appendChild(fragment)
-  canvas.appendChild(g)
+  canvas.innerHTML = `<path d="${d}"/>`
 }
 
 export function clear(canvas: SVGSVGElement) {
--- src/scripts/svg/save.ts
+++ src/scripts/svg/save.ts
@@ -1,11 +1,9 @@
 export function saveSvg(svg: SVGSVGElement, a: HTMLAnchorElement) {
-  const fileName = `qr-code.svg`
-  let blob = new Blob(['<?xml version="1.0"?>' + svg.outerHTML], {
+  const blob = new Blob(['<?xml version="1.0"?>' + svg.outerHTML], {
     type: 'image/svg+xml;charset=utf-8',
   })
-  const url = window.URL.createObjectURL(blob)
 
-  a.href = url
-  a.download = fileName
+  a.href = window.URL.createObjectURL(blob)
+  a.download = 'qr-code.svg'
   a.click()
 }
